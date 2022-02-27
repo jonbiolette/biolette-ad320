389a9afbd757caec5938077fbda1c79fbd0a50c9
Jon Biolette <jonbiolette@gmail.com>
2022-02-27 02:00:45 -0800

    Update FlashCard.js

--- client/src/components/FlashCard/FlashCard.js
+++ client/src/components/FlashCard/FlashCard.js
@@ -1,19 +1,47 @@
 import React from 'react'
-import './FlashCard.css'
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography
+} from '@mui/material'
 
-function FlashCard() {
-    return <div className='FlashCard'>
-        <img src='http://placekitten.com/200/200' />
-        <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-            totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
-            sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-            est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius
-            modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam,
-            quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-            Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur,
-            vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
-    </div>;
-
+function Flashcard({ content, previous, next, flip }) {
+  return (
+    <Box sx={{ width: '100%', mt: 8, display: 'flex', justifyContent: 'space-around'}}>
+      <Card sx={{ width: '40vw' }} elevation={3}>
+        { content.image && <CardMedia
+          component="img"
+          height="200"
+          image={content.image} />}
+        <CardContent>
+          <Typography>{content.text ?? ''}</Typography>
+        </CardContent>
+        <CardActions sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Button
+            sx={{ mx: 2 }}
+            onClick={ () => previous() }
+            variant="outlined"
+          >
+            Prev
+          </Button>
+          <Button sx={{ mx: 2 }} variant="outlined" onClick={() => flip()}>
+            Flip
+          </Button>
+          <Button
+            sx={{ mx: 2 }}
+            onClick={ () => next() }
+            variant="outlined"
+          >
+            Next
+          </Button>
+        </CardActions>
+      </Card>
+    </Box>
+  )
 }
-export default FlashCard
+
+export default Flashcard
